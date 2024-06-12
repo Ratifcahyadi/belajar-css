7bbd51d123b7e492e1811f41d82d8f66abcfd58b
Ratif Cahyadi <ratifcahyadi@students.amikom.ac.id>
2024-06-12 23:24:39 +0700

    changed 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    // "*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
